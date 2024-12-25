f72dd1a4c0dec58a7ef4ccaaa96c7eb513d4be29
arthurmkhitaryan <arthurmkhitaryan>
2024-12-25 22:40:13 +0400

    file size

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      providerOptions: {
+        local: {
+          sizeLimit: 500 * 1024 * 1024,
+        },
+      },
+    },
+  },
+});
